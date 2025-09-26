d3c15f18d8ec23cf103c35d880bad4661c6df14e
Amara <amarachiezeonyekwere@gmail.com>
2025-09-26 15:00:57 +0100

    changed port number

--- flask_todo/app.py
+++ flask_todo/app.py
@@ -47,4 +47,4 @@ with app.app_context():
 
 if __name__ == "__main__":
     # Local dev only
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
